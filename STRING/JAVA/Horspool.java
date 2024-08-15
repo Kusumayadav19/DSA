fb05ba27fde5a14ef0976b2de665b6b8ecc14051
Kusumayadav19 <kushhhyadav19@gmail.com>
2024-08-15 19:24:10 +0530

    Horspool and Naive Java

---
+++ STRING/JAVA/Horspool.java
@@ -0,0 +1,53 @@
+package DSA.STRING.JAVA;
+
+import java.util.Arrays;
+
+public class Horspool {
+
+    // Method to create the shift table
+    public static int[] shiftTable(String pattern) {
+        int m = pattern.length();
+        int[] table = new int[26]; 
+
+        Arrays.fill(table, m);
+
+        for (int i = 0; i < m - 1; i++) {
+            table[pattern.charAt(i) - 'A'] = m - 1 - i;
+        }
+
+        return table;
+    }
+
+    public static int horspool(String text, String pattern, int[] table) {
+        int n = text.length();
+        int m = pattern.length();
+        int i = m - 1;
+
+        while (i <= n - 1) {
+            int k = 0;
+            while (k < m && pattern.charAt(m - 1 - k) == text.charAt(i - k)) {
+                k++;
+            }
+
+            if (k == m) {
+                return i - m + 1;
+            } else {
+                i += table[text.charAt(i) - 'A'];
+            }
+        }
+
+    return -1;
+    }
+
+    public static void main(String[] args) {
+        String text = "GTACTATGTA";
+        String pattern = "ATGTA";
+        int[] table = shiftTable(pattern);
+
+        System.out.print("Shift Table: " + Arrays.toString(table));
+        System.out.println();
+
+        int index = horspool(text, pattern, table);
+        System.out.println("Pattern found at index: " + index);
+    }
+}
---
+++ STRING/JAVA/naive.java
@@ -0,0 +1,37 @@
+// Naive String Matching Algorithm - A brute force Approach
+package DSA.STRING.JAVA;
+
+import java.util.*;
+
+public class naive{
+
+    public static List<Integer> NaiveStringAlgo(String text, String pattern) {
+
+        List<Integer> result = new ArrayList<>();
+        int n = text.length();  // textLength 
+        int m = pattern.length(); // patternlength
+
+        for (int i = 0; i <= n -m; i++) {
+            int j = 0;
+            while(j < m && pattern.charAt(j) == text.charAt(i+j)){
+                j += 1;
+            }
+
+            if (j == m) {
+                result.add(i); 
+            }
+        }
+    return result;
+    }
+
+    public static void main(String[] args) {
+        String text = "THIS IS A TEST TEXT FOR YOU PROGRAM TO TEST";
+        String pattern = "TEST";
+
+        List<Integer> indices = NaiveStringAlgo(text, pattern);
+
+        for (int index : indices) {
+            System.out.println("Pattern found at index " + index);
+        }
+    }
+}
