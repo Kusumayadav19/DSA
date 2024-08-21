d83592913c524447534ec18509a9c5975e07c80b
Kusumayadav19 <kushhhyadav19@gmail.com>
2024-08-22 05:12:14 +0530

    Updated Fibonacci

--- DynamicProgramming/PYTHON/Fibonacci.py
+++ DynamicProgramming/PYTHON/Fibonacci.py
@@ -1,14 +1,25 @@
 # Basic Dynamic Programming using Fibonacci Series
 
-def FibonacciDP(num,cache):
+def FibMemoization(num,cache): # Memoization
     if(num == 0 or num == 1):
         return num
     if(cache[num] != 0):
         return cache[num]
 
-    cache[num] = FibonacciDP(num - 1,cache) + FibonacciDP(num -2 , cache)
+    cache[num] = FibMemoization(num - 1,cache) + FibMemoization(num -2 , cache)
     return cache[num]
+ 
+def FibTabulation(num): # Tabulation
+    Dp = [0] * (num + 1)
+    Dp[0] = 0
+    Dp[1] = 1
+    for idx in range(2,num+ 1):
+        Dp[idx] = Dp[idx-1] + Dp[idx - 2]
+
+    return Dp[num]
+
 
 num = 5
 cache =[0] * (num + 1) 
-print(FibonacciDP(num,cache))
+print("Memoization : ",FibMemoization(num,cache))
+print("Tabulation : ",FibTabulation(num))
