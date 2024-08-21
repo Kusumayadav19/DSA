ed733b5cd74622de3d1693ba4bb7b033866fabb2
Kusumayadav19 <kushhhyadav19@gmail.com>
2024-08-22 05:18:12 +0530

    Updated

--- DynamicProgramming/JAVA/Fibonacci.java
+++ DynamicProgramming/JAVA/Fibonacci.java
@@ -4,20 +4,33 @@ package DSA.DynamicProgramming.JAVA;
 
 
 public class Fibonacci {
-    public static int FibonacciDP(int num, int[] cache){
+    public static int Memoization(int num, int[] cache){
         if(num == 0 || num == 1 ){
             return num;
         }
         if(cache[num] != 0){
             return cache[num];
         }
-        cache[num] = FibonacciDP(num-1, cache) + FibonacciDP(num - 2, cache);
+        cache[num] = Memoization(num-1, cache) + Memoization(num - 2, cache);
 
     return cache[num];
     }
+    public static int Tabulation(int num){
+        int[] Dp = new int[num+1];
+        Dp[0] = 0;
+        Dp[1] = 1;
+
+        for(int n = 2; n < num + 1; n++){
+            Dp[n] = Dp[n-1] + Dp[n - 2];
+        }
+
+    return Dp[num];
+    }
+
     public static void main(String[] args) {
         int num = 5;
         int[] cache = new int[num+1];
-        System.out.println(FibonacciDP(num, cache));
+        System.out.println("Memoization : " +Memoization(num, cache));
+        System.out.println("Tabulation : "+Tabulation(num));
     }
 }
